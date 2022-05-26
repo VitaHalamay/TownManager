5f9c53b2d18e243db37ab5285f4f276b1ecc24a4
Vita <vitahalamay@gmail.com>
2022-05-26 21:44:44 +0300

    UI enhancements

--- TownManager/Models/Enums/BuildingType.cs
+++ TownManager/Models/Enums/BuildingType.cs
@@ -16,4 +16,39 @@
         TextileCompany
     }
 
+    public static class BuildingTypeExtentions
+    {
+        public static string GetName(this BuildingType buildingType)
+        {
+            switch (buildingType)
+            {
+                case BuildingType.ElectronicsCompany: 
+                    return "Electronics";
+                case BuildingType.FireStation:
+                    return "Fire station";
+                case BuildingType.Hospital:
+                    return "Hospital";
+                case BuildingType.Hotel:
+                    return "Hotel";
+                case BuildingType.House:
+                    return "House";
+                case BuildingType.MetallurgyCompany:
+                    return "Metallurgy";
+                case BuildingType.Museum:
+                    return "Museum";
+                case BuildingType.PharmaceuticalCompany:
+                    return "Pharmaceutics";
+                case BuildingType.PoliceStation:
+                    return "Police station";
+                case BuildingType.ProvisionCompany:
+                    return "Provision";
+                case BuildingType.Store:
+                    return "Store";
+                case BuildingType.TextileCompany:
+                    return "Textile";
+            }
+
+            return "";
+        }
+    }
 }
--- TownManager/Models/Enums/StatisticsType.cs
+++ TownManager/Models/Enums/StatisticsType.cs
@@ -7,9 +7,9 @@ namespace TownManager.Models.Enums
 {
     public enum StatisticsType
     {
+        MoneyCount,
         CitizensCount,
         WorkplacesCount,
-        MoneyCount,
 
         CultureLevel,
         ProvisionLevel,
@@ -24,5 +24,39 @@ namespace TownManager.Models.Enums
 
 
     }
+    public static class StatisticsTypeExtentions
+    {
+        public static string GetName(this StatisticsType statisticsType)
+        {
+            switch (statisticsType)
+            {
+                case StatisticsType.CitizensCount:
+                    return "Citizens";
+                case StatisticsType.WorkplacesCount:
+                    return "Workplaces";
+                case StatisticsType.MoneyCount:
+                    return "Money";
+                case StatisticsType.CultureLevel:
+                    return "Culture level";
+                case StatisticsType.ProvisionLevel:
+                    return "Provision level";
+                case StatisticsType.SecurityLevel:
+                    return "Security level";
+                case StatisticsType.PrestigeLevel:
+                    return "Prestige level";
+                case StatisticsType.HealthLevel:
+                    return "Health level";
+                case StatisticsType.MedicinesCount:
+                    return "Medicines";
+                case StatisticsType.DevicesCount:
+                    return "Devices";
+                case StatisticsType.SparePartsCount:
+                    return "Spare parts";
+                case StatisticsType.ClothesCount:
+                    return "Clothes";
+            }
 
+            return "";
+        }
+    }
 }
