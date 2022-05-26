a6e8f5c7448b30808e6e7e4f89059ca86f0f9815
Vita <vitahalamay@gmail.com>
2022-05-26 20:16:06 +0300

    Implemented flyweight pattern

---
+++ TownManager/Services/Patterns/Flyweight.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using TownManager.Models.Enums;
+
+namespace TownManager.Services.Patterns
+{
+    public class Flyweight
+    {
+        private Dictionary<BuildingType, string> Images = new Dictionary<BuildingType, string>();
+
+        public string GetImage(BuildingType type)
+        {
+            if (!Images.ContainsKey(type))
+            {
+                var path = $"wwwroot/Images/{type}.png";
+                byte[] content = File.ReadAllBytes(path);
+                Images.Add(type, Convert.ToBase64String(content));
+            }
+
+            return Images[type];
+        }
+    }
+}
